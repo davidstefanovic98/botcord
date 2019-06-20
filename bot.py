da589d97477f0b72f9b753008871237785c697f5
Nikola Tasic <ntasic7@gmail.com>
2019-06-20 19:18:10 +0200

    initial

---
+++ bot.py
@@ -0,0 +1,103 @@
+import discord
+from gitapi import GitApi
+from os.path import exists
+from configparser import ConfigParser
+
+cfgfilepth = "auth.cfg"
+
+
+class JoniBot(discord.Client):
+	config = ConfigParser()
+	token = None
+	subs = {}
+
+	def __init__(self, *args, **kwargs):
+		if exists(cfgfilepth):
+			self.config.read(cfgfilepth)
+		else:
+			raise FileExistsError("Config file auth.cfg not found in root directory")
+
+		if "auth" in self.config and "token" in self.config["auth"]:
+			self.token = self.config["auth"]["token"]
+		else:
+			raise IOError("Token not found in auth.cfg file")
+		super().__init__(**kwargs)
+
+		self.run(self.token)
+
+	async def on_ready(self):
+		print('Logged on as {0}!'.format(self.user))
+
+	async def on_message(self, message):
+		if self.user == message.author:
+			return
+
+		if message.content.startswith("$"):
+			await self.do_command(message)
+
+	async def do_command(self, message):
+		args = message.content[1:].split(" ")
+		if args[0] == "git":
+			if args[1] == "sub" or args[1] == "subscribe":
+				await self.subscribe(message)
+				return
+			elif args[1] == "unsub" or args[1] == "unsubscribe":
+				await self.unsubscribe(message)
+				return
+
+			user_id = str(message.author.id)
+
+			if user_id not in self.subs:
+				await message.channel.send("**You are not subscribed to a repository**")
+				return
+
+			try:
+				git = GitApi(self.subs[user_id]["user"],
+				             self.subs[user_id]["passwd"],
+				             self.subs[user_id]["repo"])
+			except IOError:
+				await message.channel.send("**You are not subscribed to a repository**")
+				return
+
+			if args[1] == "commits" or args[1] == "commit":
+
+				if len(args) == 3:
+					await message.channel.send(git.get_commit(args[2]))
+
+				elif len(args) == 2:
+					await message.channel.send(git.get_commit())
+
+			if args[1] == "branches" or args[1] == "branch":
+
+				if len(args) == 3:
+					await message.channel.send(git.get_branch(args[2]))
+
+				elif len(args) == 2:
+					await message.channel.send(git.get_branch())
+
+	async def subscribe(self, message):
+		args = message.content[1:].split(" ")
+		if len(args) == 5:
+			user = args[2]
+			passwd = args[3]
+			repo = args[4]
+			user_id = str(message.author.id)
+			self.subs[user_id] = {}
+			self.subs[user_id]["user"] = user
+			self.subs[user_id]["passwd"] = passwd
+			self.subs[user_id]["repo"] = repo
+			await message.delete()
+			await message.channel.send("**GG, {} subscribed to {} github repository**".format(user, repo))
+		else:
+			message.delete()
+			await message.channel.send("**Invalid subscription arguments**")
+
+	async def unsubscribe(self, message):
+		user_id = str(message.author.id)
+		repo = self.subs[user_id]["repo"]
+		del self.subs[user_id]
+		await message.delete()
+		await message.channel.send("**{} unsubscribed fron {}**".format(message.author, repo))
+
+
+client = JoniBot()
---
+++ gitapi.py
@@ -0,0 +1,83 @@
+import json
+
+import requests
+
+
+class GitApi(object):
+	def __init__(self, user=None, passwd=None, repo=None) -> None:
+		super().__init__()
+		if user is None or passwd is None or repo is None:
+			raise IOError("Invalid arguments")
+		self.user = user
+		self.passwd = passwd
+		self.repo = repo
+
+	def get_commit(self, sha=None) -> str:
+		if sha is not None:
+			url = "https://api.github.com/repos/{user}/{repo}/commits/{sha}".format(user=self.user, passwd=self.passwd,
+			                                                                        repo=self.repo, sha=sha)
+		else:
+			url = "https://api.github.com/repos/{user}/{repo}/commits".format(user=self.user, passwd=self.passwd,
+			                                                                  repo=self.repo)
+
+		response = requests.get(url, params={"username": self.user, "password": self.passwd})
+
+		if response.status_code == 422:
+			return "**Not a valid commit hash**"
+
+		data = json.loads(response.content)
+		message = ""
+
+		if sha is not None:
+			message += "Author: **{}** ({})\n".format(data["commit"]["author"]["name"], data["author"]["login"])
+			message += "Message: {}\n".format(data["commit"]["message"])
+			message += "Date: {}\n".format(data["commit"]["author"]["date"])
+			message += "URL: <{}>\n".format(data["html_url"])
+			message += "Files changed: \n\n"
+			for file in data["files"]:
+				message += "Path: " + file["filename"] + "\n"
+				message += "Status: " + file["status"] + "\n"
+				message += "Changes: " + str(file["changes"]) + "\n\n"
+
+			return message
+		else:
+			message += "Latest 5 commits to any branch: \n\n"
+			for i in range(0, 5):
+				commit = data[i]
+				message += "Author: **{}** ({})\n".format(commit["commit"]["author"]["name"], commit["author"]["login"])
+				message += "Message: {}\n".format(commit["commit"]["message"])
+				message += "sha: _{}_\n".format(commit["sha"])
+				message += "URL: _{}_\n\n".format(commit["html_url"])
+			return message
+
+	def get_branch(self, branch=None) -> str:
+		if branch is not None:
+			url = "https://api.github.com/repos/{user}/{repo}/branches/{branch}".format(user=self.user,
+			                                                                            passwd=self.passwd,
+			                                                                            repo=self.repo, branch=branch)
+		else:
+			url = "https://api.github.com/repos/{user}/{repo}/branches".format(user=self.user, passwd=self.passwd,
+			                                                                   repo=self.repo)
+
+		response = requests.get(url, params={"username": self.user, "password": self.passwd})
+
+		print(response.status_code)
+
+		if response.status_code == 422:
+			return "**Not a valid branch**"
+
+		data = json.loads(response.content)
+		message = ""
+
+		if branch is None:
+			message += "List of all active branches:\n\n"
+			for b in data:
+				message += "Branch: **" + b["name"] + "**\n"
+				message += "SHA: _" + b["commit"]["sha"] + "_\n\n"
+			return message
+		else:
+			message += "Branch: **" + data["name"] + "**\n"
+			message += "Latest commit: **" + data["commit"]["commit"]["message"] + "**\n"
+			message += "SHA: _" + data["commit"]["sha"] + "_\n"
+			message += "URL: _" + data["commit"]["html_url"] + "_ \n\n"
+			return message
